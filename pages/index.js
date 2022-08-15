b8db72a95f3d74d3df3d1ac34ac72bc85c9b56c7
enkog <nkirukaogu@gmail.com>
2022-08-15 12:05:28 +0100

    Add components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Product, FooterBanner, HeroBanner } from "../components";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <HeroBanner />
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className="products-container">
+        {["Product 1", "Product 2"].map((product) => product)}
+      </div>
+      <FooterBanner />
+    </div>
+  );
 };
 
 export default Home;
